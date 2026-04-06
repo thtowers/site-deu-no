b209cc53f28f547026b7e27cc4d4294c484e0016
Thiago Torres <thtowers@gmail.com>
2026-04-05 21:34:40 -0300

    feat: add new color assets and update colors catalog and product section

--- src/components/ProductsSection.jsx
+++ src/components/ProductsSection.jsx
@@ -30,10 +30,10 @@ const ProductsSection = () => {
 
                 {/* Produto Rastro */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['verde_militar', 'verde_bandeira', 'azul_marinho', 'castanho', 'verde_jade']}
                     id="colares"
                     name="Colar Rastro"
-                    description={"Forte, marcante, inesquecível — como você."}
+                    description={"Forte, marcante, inesquecível — como você."} F
                     price=" R$40,00"
                     imageSrc={["/assets/produtos/Rastro.webp", "/assets/produtos/Rastro2.webp"]}
                     mobileImageSrc="/assets/produtos/Rastro.webp"
@@ -46,7 +46,7 @@ const ProductsSection = () => {
 
                 {/* Produto Fluxo */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['prata_e_dourado']}
                     name="Colar Fluxo"
                     description={"Um colar, infinitas versões de você. "}
                     price=" R$ 45,00"
@@ -61,7 +61,7 @@ const ProductsSection = () => {
 
                 {/* Produto Traço */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['preto', 'terracota', 'marrom']}
                     name="Colar Traço"
                     description={["A força do simples bem definido. "]}
                     price="R$ 58,00"
@@ -75,7 +75,7 @@ const ProductsSection = () => {
 
                 {/* Produto Ângulo */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['bordo_sf', 'azul_marinho_sf', 'bege_natural', 'marrom_sf', 'preto_sf', 'turquesa']}
                     name="Colar Ângulo "
                     description={["Impacto sutil, elegância absoluta. "]}
                     price="R$ 60,00"
@@ -89,7 +89,16 @@ const ProductsSection = () => {
 
                 {/* Produto Ponto */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['areia', 'cinza', 'azul_bebe',
+                        'amarelo_manteiga', 'verde_limao', 'rosa_bebe',
+                        'verde_militar', 'azul_petroleo', 'verde_bandeira',
+                        'azul_marinho', 'azul_anil', 'azul_royal', 'roxo',
+                        'rosa', 'laranja', 'vermelho', 'terracota',
+                        'vermelho_figo', 'vinho', 'castanho', 'marrom',
+                        'chumbo', 'mostarda', 'verde_jade', 'preto_poa_branco',
+                        'preto', 'azul_marinho_poa_branco', 'verde_militar_poa_branco',
+                        'rami_branco', 'areia_poa_marrom_escuro', 'vermelho_poa_azul_marinho'
+                    ]}
                     name="Colar Ponto "
                     description={["Quem tem estilo, sabe: é tudo sobre o ponto certo."]}
                     price="R$ 60,00"
@@ -103,7 +112,11 @@ const ProductsSection = () => {
 
                 {/* Produto Vínculo */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['lenço_azul_bebe', 'lenço_vermelho', 'lenço_preto', 'lenço_branco',
+                        'lenço_vermelho_vivo', 'lenço_bege', 'lenço_marrom_escuro', 'lenço_azul_marinho',
+                        'lenço_verde', 'lenço_verde_escuro'
+
+                    ]}
                     name="Colar Vínculo "
                     description={["moderno no design, forte na personalidade. "]}
                     price="R$ 65,00"
@@ -117,7 +130,14 @@ const ProductsSection = () => {
 
                 {/* Produto Eixo */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['areia', 'cinza', 'azul_bebe', 'amarelo_manteiga', 'verde_limao',
+                        'rosa_bebe', 'verde_militar', 'azul_petroleo', 'verde_bandeira', 'azul_marinho',
+                        'azul_anil', 'azul_royal', 'roxo', 'rosa', 'laranja', 'vermelho', 'terracota',
+                        'vermelho_figo', 'vinho', 'castanho', 'marrom', 'chumbo', 'mostarda', 'verde_jade',
+                        'preto', 'preto_poa_branco', 'azul_marinho_poa_branco', 'verde_militar_poa_branco',
+                        'rami_branco', 'areia_poa_marrom_escuro', 'vermelho_poa_azul_marinho'
+
+                    ]}
                     name="Colar Eixo"
                     description={["O equilíbrio entre quem você é e o que você mostra. "]}
                     price="R$ 70,00"
@@ -131,7 +151,8 @@ const ProductsSection = () => {
 
                 {/* Produto Ciclo */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['bordo_sf', 'azul_marinho_sf', 'bege_natural_sf',
+                        'marrom_sf', 'preto_sf', 'turquesa']}
                     name="Colar Ciclo"
                     description={["Para quem transforma presença em assinatura. "]}
                     price="R$ 80,00"
@@ -145,7 +166,14 @@ const ProductsSection = () => {
 
                 {/* Produto Eco */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['areia', 'cinza', 'azul_bebe', 'amarelo_manteiga', 'verde_limao', 'rosa_bebe',
+                        'verde_militar', 'azul_petroleo', 'verde_bandeira', 'azul_marinho', 'azul_anil', 'azul_royal',
+                        'roxo', 'rosa', 'laranja', 'vermelho', 'terracota', 'vermelho_figo', 'vinho', 'castanho',
+                        'marrom', 'chumbo', 'mostarda', 'verde_jade', 'marrom_escuro', 'preto', 'preto_poa_branco',
+                        'azul_marinho_poa_branco', 'verde_militar_poa_branco', 'rami_branco', 'areia_poa_marrom_escuro',
+                        'vermelho_poa_azul_marinho'
+
+                    ]}
                     name="Colar Eco"
                     description={["Elegante hoje, atemporal sempre. "]}
                     price="R$ 85,00"
@@ -159,7 +187,12 @@ const ProductsSection = () => {
 
                 {/* Produto Elo */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['areia', 'cinza', 'azul_bebe', 'amarelo_manteiga', 'verde_limao',
+                        'rosa_bebe', 'verde_militar', 'azul_petroleo', 'verde_bandeira', 'azul_marinho',
+                        'azul_anil', 'azul_royal', 'roxo', 'rosa', 'laranja', 'vermelho', 'terracota',
+                        'vermelho_figo', 'vinho', 'castanho', 'marrom', 'chumbo', 'mostarda', 'verde_jade',
+                        'preto', 'preto_poa_branco', 'azul_marinho_poa_branco', 'verde_militar_poa_branco',
+                        'rami_branco', 'areia_poa_marrom_escuro', 'vermelho_poa_azul_marinho']}
                     id="pulseiras"
                     name="Pulseira Elo"
                     description={"Presença que se impõe, estilo que permanece. "}
@@ -174,7 +207,12 @@ const ProductsSection = () => {
 
                 {/* Produto Orvalho */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['areia', 'cinza', 'azul_bebe', 'amarelo_manteiga', 'verde_limao',
+                        'rosa_bebe', 'verde_militar', 'azul_petroleo', 'verde_bandeira', 'azul_marinho',
+                        'azul_anil', 'azul_royal', 'roxo', 'rosa', 'laranja', 'vermelho', 'terracota',
+                        'vermelho_figo', 'vinho', 'castanho', 'marrom', 'chumbo', 'mostarda', 'verde_jade',
+                        'preto', 'preto_poa_branco', 'azul_marinho_poa_branco', 'verde_militar_poa_branco',
+                        'rami_branco', 'areia_poa_marrom_escuro', 'vermelho_poa_azul_marinho']}
                     name="Pulseira Orvalho"
                     description={"Sutil no olhar, marcante no sentir. "}
                     price="R$ 45,00"
@@ -188,7 +226,10 @@ const ProductsSection = () => {
 
                 {/* Produto Laço */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['terracota', 'verde_bandeira', 'marrom_escuro', 'vinho', 'preto', 'verde_militar',
+                        'verde_jade', 'azul_marinho', 'areia', 'mostarda', 'castanho', 'verde_limao', 'amarelo_manteiga',
+                        'vermelho_figo', 'marrom', 'rosa_bebe', 'chumbo', 'cinza', 'laranja', 'rami'
+                    ]}
                     name="Pulseira Laço"
                     description={"Feito para destacar quem você é. "}
                     price="R$ 50,00"
@@ -202,7 +243,12 @@ const ProductsSection = () => {
 
                 {/* Produto Brinco Amá */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['rami', 'azul_marinho', 'chumbo', 'verde_bandeira', 'areia', 'preto',
+                        'verde_jade', 'mostarda', 'marrom', 'marrom_escuro', 'castanho', 'vinho',
+                        'vermelho_figo', 'terracota', 'vermelho', 'rosa', 'roxo', 'azul_royal', 'verde_militar',
+                        'rosa_bebe', 'azul_bebe', 'cinza'
+
+                    ]}
                     id="brincos"
                     name="Brinco Amá"
                     description={"Onde o atemporal encontra a sua força."}
@@ -217,7 +263,13 @@ const ProductsSection = () => {
 
                 {/* Produto Brinco Bae */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['areia', 'cinza', 'azul_bebe', 'amarelo_manteiga', 'verde_limao', 'rosa_bebe',
+                        'verde_militar', 'azul_petroleo', 'verde_bandeira', 'azul_marinho', 'azul_anil', 'azul_royal',
+                        'roxo', 'rosa', 'laranja', 'vermelho', 'terracota', 'vermelho_figo', 'vinho', 'castanho',
+                        'marrom', 'chumbo', 'mostarda', 'verde_jade', 'preto', 'preto_poa_branco',
+                        'azul_marinho_poa_branco', 'verde_militar_poa_branco', 'rami_branco', 'areia_poa_marrom_escuro',
+                        'vermelho_poa_azul_marinho'
+                    ]}
                     name="Brinco Bae"
                     description={"Bae prova: o essencial também impõe presença."}
                     price="R$ 30,00"
@@ -231,7 +283,12 @@ const ProductsSection = () => {
 
                 {/* Produto Brinco Douré */}
                 <ProductCard
-                    colorOptions={['azul_marinho', 'bordo', 'bege_natural', 'marrom', 'turquesa', 'preto']}
+                    colorOptions={['areia', 'cinza', 'azul_bebe', 'amarelo_manteiga', 'verde_limao', 'rosa_bebe',
+                        'verde_militar', 'azul_petroleo', 'verde_bandeira', 'azul_marinho', 'azul_anil', 'azul_royal',
+                        'roxo', 'rosa', 'laranja', 'vermelho', 'terracota', 'vermelho_figo', 'vinho', 'castanho',
+                        'marrom', 'chumbo', 'mostarda', 'verde_jade', 'marrom_escuro', 'preto', 'preto_poa_branco',
+                        'azul_marinho_poa_branco', 'verde_militar_poa_branco', 'rami_branco', 'areia_poa_marrom_escuro',
+                        'vermelho_poa_azul_marinho']}
                     name="Brinco Douré"
                     description={"Douré: menos dúvida, mais presença."}
                     price="R$ 35,00"
